163aca3533df0276573a132ec2bfbd75263fc6c4
AgusQuintanar <agusquintanar17@gmail.com>
2020-09-26 22:52:52 -0500

    updated gitignore

---
+++ frontend/build/precache-manifest.aad2522cd0ecee6b29dd4102a35b682a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8591e992214ee003742c1578ed8bffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "759ec20f6c5663af4a2b",
+    "url": "/static/css/2.7a351efc.chunk.css"
+  },
+  {
+    "revision": "ddd38e371d87cff648c8",
+    "url": "/static/css/main.b0a59593.chunk.css"
+  },
+  {
+    "revision": "759ec20f6c5663af4a2b",
+    "url": "/static/js/2.6ca04aa5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ca04aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd38e371d87cff648c8",
+    "url": "/static/js/main.cebd1688.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
